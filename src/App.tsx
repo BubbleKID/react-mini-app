2422a4d0c8ef9c98d366d8a6c02ce386f8acf029
Mark Chen <xin.chen@chemistwarehouse.com.au>
2024-10-22 00:51:27 +1100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react';
+import { ImageType } from './types';
+ 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [images, setImages] = useState<ImageType[]>([])
+
+  useEffect(() => {
+    fetch('https://picsum.photos/v2/list?page=2&limit=10')
+      .then(response => response.json())
+      .then(data => setImages(data));
+  }, []);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh', width: '50vw', border: '1px solid red' }}>
+          <h1>Im the viewport</h1>
+          <LazyImageContainer images={images} />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
+}
+
+const LazyImageContainer = (props: { images: ImageType[] }) => {
+  return <>
+    {
+      props.images.map((image: ImageType) => {
+        return <LazyImage key={image.id} {...image} />
+      })
+    }
+  </>
+}
+
+const LazyImage = (props: ImageType) => {
+  return <img src={props.download_url} width={200} height={200} />
 }
 
 export default App
