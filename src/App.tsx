e19d9934e09ccff5ad0ae2661454794131e58c8e
Mark Chen <xin.chen@chemistwarehouse.com.au>
2024-10-22 21:29:12 +1100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,35 +7,56 @@ function App() {
   const [images, setImages] = useState<ImageType[]>([])
 
   useEffect(() => {
-    fetch('https://picsum.photos/v2/list?page=2&limit=10')
+    fetch('https://picsum.photos/v2/list?page=2&limit=20')
       .then(response => response.json())
       .then(data => setImages(data));
   }, []);
 
   return (
-    <>
-      <div>
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh', width: '50vw', border: '1px solid red' }}>
-          <h1>Im the viewport</h1>
+    <div>
+      <h1>Im the window</h1>
+        <div style={{ 
+            display: 'flex', 
+            height: '50vh', 
+            width: '50vw', 
+            border: '5px solid red',
+            position: 'fixed',
+            zIndex: 200,
+          }}>
+        </div>
+        <div style={{
+          height: '50vh', 
+          width: '50vw', 
+        }}>
           <LazyImageContainer images={images} />
         </div>
-      </div>
-    </>
+    </div>
   )
 }
 
 const LazyImageContainer = (props: { images: ImageType[] }) => {
-  return <>
+  return <div style={{ 
+    display: 'flex', 
+    flexWrap: 'wrap', 
+    justifyContent: 'flex-start', 
+    alignItems: 'flex-start', 
+    
+  }}>
     {
       props.images.map((image: ImageType) => {
         return <LazyImage key={image.id} {...image} />
       })
     }
-  </>
+  </div>
 }
 
 const LazyImage = (props: ImageType) => {
-  return <img src={props.download_url} width={200} height={200} />
+  return <img 
+    src={props.download_url} 
+    width={200} 
+    height={200} 
+    style={{ objectFit: 'cover', zIndex: 100 }}
+  />
 }
 
 export default App
