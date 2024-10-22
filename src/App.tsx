d34d87815c8eed26ee5ed08b2eaf4ebd61054f69
Mark Chen <xin.chen@chemistwarehouse.com.au>
2024-10-23 01:26:24 +1100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -21,16 +21,20 @@ function App() {
             height: '50vh', 
             width: '50vw', 
             border: '5px solid red',
-            // position: 'fixed',
-            //zIndex: 100,
+            position: 'fixed',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            overflow: 'scroll',
+            zIndex: 10,
           }}>
-            <LazyImageContainer images={images} />
+            <LazyImageContainer images={images} scrollAreaRef={scrollAreaRef} />
         </div>
     </div>
   )
 }
 
-const LazyImageContainer = (props: { images: ImageType[] }) => {
+const LazyImageContainer = (props: { images: ImageType[], scrollAreaRef: React.RefObject<HTMLDivElement> }) => {
   return <div  style={{ 
     display: 'flex', 
     flexWrap: 'wrap', 
@@ -39,19 +43,19 @@ const LazyImageContainer = (props: { images: ImageType[] }) => {
   }}>
     {
       props.images.map((image: ImageType) => {
-        return <LazyImage key={image.id} {...image} />
+        return <LazyImage key={image.id} {...image}  scrollAreaRef={props.scrollAreaRef}/>
       })
     }
   </div>
 }
 
-const LazyImage = (props: ImageType) => {
+const LazyImage = (props: ImageType & { scrollAreaRef: React.RefObject<HTMLDivElement> }) => {
   const imageRef = useRef<HTMLDivElement>(null);
   const [isIntersecting, setIsIntersecting] = useState(false);
 
   useEffect(() => {
     const options = {
-      root: document.querySelector("#scrollArea"),
+      root: props.scrollAreaRef.current,
       rootMargin: "0px",
       threshold: 0.1,
     };
@@ -79,18 +83,20 @@ const LazyImage = (props: ImageType) => {
         observer.unobserve(imageRef.current);
       }
     }
-  }, []);
+  }, [isIntersecting]);
 
-  return <div ref={imageRef} style={{ width: '33.33%', height: '200px' }}>
-    { isIntersecting ? <img 
-      src={ props.download_url } 
-      width={200} 
-      height={200} 
-      style={{ objectFit: 'cover' }}
-    /> :  (
+  return (
+    <div ref={imageRef} style={{ width: '33.33%', height: '200px', flexShrink: 0, zIndex: 20 }}>
+      { isIntersecting ? <img 
+        src={ props.download_url } 
+        width={200} 
+        height={200} 
+        style={{ width: '100%', height: '100%',objectFit: 'cover' }}
+      /> :  (
       <div style={{ width: '100%', height: '100%', background: '#f0f0f0', border: '5px solid blue', }} />
-    )}
-  </div>
+      )}
+    </div>
+  )
 }
 
 export default App
